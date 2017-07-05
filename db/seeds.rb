ffd706f4d64f7b8bd0bf95f87be244e4f0ca54ee
Amanda Beiner <aebeiner@gmail.com>
2017-07-05 16:06:44 -0400

    working app without tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Gif.create(name: "Hello this is Dog", url: "https://media.giphy.com/media/pSpmpxFxFwDpC/giphy.gif", likes: 25)
+
+Gif.create(name: "Pug Swimming", url: "https://media.giphy.com/media/r6ALgGVKLg93O/giphy.gif", likes: 47)
+
+Gif.create(name: "Hot Dogs", url: "https://media.giphy.com/media/NmGbJwLl7Y4lG/giphy.gif", likes: 19)
+
+Gif.create(name: "German Shepherd Ears", url: "https://media.giphy.com/media/WoLfYYgO14UTu/giphy.gif", likes: 42)
